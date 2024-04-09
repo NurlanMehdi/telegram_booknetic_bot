548d33fdb6c2bb92ef29901e7b4e607c39fb5642
nurlan <nurlan>
2024-04-09 14:47:53 +0200

    Last Commit

--- .history/public/index_20240409143633.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include_once __DIR__ . '/../controllers/TelegramController.php';
-// Run the project using README.md
-
-$telegramController = new TelegramController();
-$telegramController->handleIncomingMessages();
-header("Refresh: 1");
